9d94c2de5757da18e6cfa748392c822996ce1af2
Firoj Ahmed <robin.promoteur@gmail.com>
2026-02-10 02:12:57 +0600

    Static URL bug fix.

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,32 @@
-
 import express, { Application } from 'express';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
-import cookieParser from "cookie-parser";
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { notFound } from './middlewares/notFound';
 import { RootRouter } from './routes/rootRouter';
 import { globalErrorHandler } from './middlewares/errorHandler';
-import seed from './lib/seed';
- 
+//import seed from './lib/seed';
+import path from 'path';
 
 const app: Application = express();
 
 app.use(cookieParser());
+
 // Middleware
-app.use(cors({
-  origin: "http://localhost:3000", // your frontend URL
-  credentials: true // allow cookies to be sent
-}));
+app.use(
+  cors({
+    origin: 'http://localhost:3000', // your frontend URL
+    credentials: true, // allow cookies to be sent
+  }),
+);
+
 app.use(express.json());
+
+app.use('/api/v1/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
 // Routes
 app.use('/api/v1', RootRouter);
-
 
 // Health check
 app.get('/health', (req, res) => res.status(200).json({ status: 'ok' }));
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,68 +1,62 @@
-
-import { TUserPayload } from "../../types/user";
-import { catchAsync } from "../../utils/catchAsync";
-import { UserService } from "./user.service";
-import { UserProfileValidation } from "./user.validation";
+import { TUserPayload } from '../../types/user';
+import { catchAsync } from '../../utils/catchAsync';
+import { UserService } from './user.service';
+// import { UserProfileValidation } from "./user.validation";
 // import { profileSchema } from "./user.validation";
-
-
-
 
 ////// Profile create /////////
 
 const me = catchAsync(async (req, res) => {
-  const user = req.user
+  const user = req.user;
 
-  const result = await UserService.me(user as TUserPayload)
+  const result = await UserService.me(user as TUserPayload);
 
   res.status(201).json({
     status: true,
-    message: "get my profile successfully",
-    data: result
-  })
-})
-
+    message: 'get my profile successfully',
+    data: result,
+  });
+});
 
 const createCandidatePersonal = catchAsync(async (req, res) => {
-
-  const user = req.user
-  const result = await UserService.createCandidatePersonalService(req.body, user as TUserPayload)
+  const user = req.user;
+  const result = await UserService.createCandidatePersonalService(
+    req.body,
+    user as TUserPayload,
+  );
   res.status(201).json({
     status: true,
-    message: "Profile created successfully",
-    data: result
-  })
-})
+    message: 'Profile created successfully',
+    data: result,
+  });
+});
 
 const createCandidateExperience = catchAsync(async (req, res) => {
-
-  const result = await UserService.createCandidateExperienceService(req.body, req.user as TUserPayload)
+  const result = await UserService.createCandidateExperienceService(
+    req.body,
+    req.user as TUserPayload,
+  );
   res.status(201).json({
     status: true,
-    message: "Created user Exprience successfully",
-    data: result
-  })
-})
-
+    message: 'Created user Exprience successfully',
+    data: result,
+  });
+});
 
 const dropdown = catchAsync(async (req, res) => {
-
-  const result = await UserService.dropdown()
+  const result = await UserService.dropdown();
   res.status(201).json({
     status: true,
-    message: "get all personal dropdown successfully",
-    data: result
-  })
-})
-
-
+    message: 'get all personal dropdown successfully',
+    data: result,
+  });
+});
 
 // const createCertificate = catchAsync(async (req, res) => {
 //   const files = req.files;
 
 //   const certNames = Array.isArray(req.body.certNames) ? req.body.certNames : req.body.certNames?.split(","); // if sent as comma-separated string
 //   const user = req.user
-
 
 //   const result = await UserService.createCertificate(user as TUserPayload, files as Express.Multer.File[], certNames)
 
@@ -77,9 +71,7 @@ const dropdown = catchAsync(async (req, res) => {
 
 ////// Dropdown Data ///////////////
 
-
 // const getDivisionWithDistrictsAndUpazilas = catchAsync(async (req, res) => {
-
 
 //   const result = await UserService.getDivisionWithDistrictsAndUpazilas(req.query as {divisionId: string, districtId: string, upazilaId:string})
 
@@ -91,28 +83,24 @@ const dropdown = catchAsync(async (req, res) => {
 // })
 
 const createCandidateEducation = catchAsync(async (req, res) => {
-
-  const result = await UserService.createCandidateEducationService(req.body, req.user as TUserPayload)
+  const result = await UserService.createCandidateEducationService(
+    req.body,
+    req.user as TUserPayload,
+  );
   res.status(201).json({
     status: true,
-    message: "Created user Education successfully",
-    data: result
-  })
-})
-
+    message: 'Created user Education successfully',
+    data: result,
+  });
+});
 
 export const UserController = {
   createCandidatePersonal,
   createCandidateExperience,
   me,
-  
-  //// Dropdown query 
-  // getDivisionWithDistrictsAndUpazilas, 
-  dropdown, 
-  createCandidateEducation
-}
 
-
-
-
-
+  //// Dropdown query
+  // getDivisionWithDistrictsAndUpazilas,
+  dropdown,
+  createCandidateEducation,
+};
